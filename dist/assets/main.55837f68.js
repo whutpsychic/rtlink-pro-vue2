7df05e9ffb77e7b0a21490d79e7f08a0d0c6a220
zbc <1511828529@qq.com>
2023-11-18 10:32:05 +0800

    test dist

---
+++ dist/assets/main.55837f68.js
@@ -0,0 +1,12 @@
+import{n as t}from"./index.9dfeebb5.js";const r={data(){return{install:"npm i rtlink-pro-vue2 -s",code:`import Vue from 'vue';
+import RtlinkUIPro from 'rtlink-pro-vue2';
+import App from './App.vue';
+
+Vue.use(RtlinkUIPro);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});`}}};var i=function(){var e=this,n=e._self._c;return n("div",{staticClass:"page-content-container"},[n("h2",[e._v("\u5FEB\u901F\u4E0A\u624B")]),n("p",[e._v("\u672C\u8282\u5C06\u4ECB\u7ECD\u5982\u4F55\u5728\u9879\u76EE\u4E2D\u4F7F\u7528 rtlink-pro\u3002")]),n("h3",[e._v("\u4F7F\u7528 npm \u5B89\u88C5")]),n("pre",{directives:[{name:"highlightjs",rawName:"v-highlightjs"}]},[e._v("    "),n("code",{staticClass:"javascript"},[e._v(e._s(e.install))]),e._v(`
+  `)]),n("h3",[e._v("\u5F15\u5165\u7EC4\u4EF6")]),n("p",[e._v("\u5728 main.js \u4E2D\u5199\u5165\u4EE5\u4E0B\u5185\u5BB9\uFF1A")]),n("pre",{directives:[{name:"highlightjs",rawName:"v-highlightjs"}]},[e._v("    "),n("code",{staticClass:"javascript"},[e._v(e._s(e.code))]),e._v(`
+  `)]),n("p",[e._v(" \u4EE5\u4E0A\u4EE3\u7801\u4FBF\u5B8C\u6210\u4E86 rtlink-pro \u7684\u5F15\u5165\u3002")])])},a=[],s=t(r,i,a,!1,null,"81f731a0",null,null);const p=s.exports;export{p as default};
