3442f1870251179c3cd5a060edc6618d1ff8193e
zbc <1511828529@qq.com>
2023-11-20 17:30:43 +0800

    fix 显示 bug

--- dist/assets/main.77d2affc.js
+++ dist/assets/main.77d2affc.js
@@ -1,4 +1,4 @@
-import{n as t}from"./index.cb4da614.js";const r={data(){return{install:"npm i rtlink-pro-vue2 -s",code:`import Vue from 'vue';
+import{n as t}from"./index.c37c3227.js";const r={data(){return{install:"npm i rtlink-pro-vue2 -s",code:`import Vue from 'vue';
 import RtlinkUIPro from 'rtlink-pro-vue2';
 import App from './App.vue';
 
